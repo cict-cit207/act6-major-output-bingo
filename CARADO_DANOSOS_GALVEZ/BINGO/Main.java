c764697eecbab4efd4c54944b9a3de77a666c581
hydraadra112 <johnmanuel.carado@wvsu.edu.ph>
2023-12-19 23:59:24 +0800

    able to play multiple games with different card each game

--- CARADO_DANOSOS_GALVEZ/BINGO/Main.java
+++ CARADO_DANOSOS_GALVEZ/BINGO/Main.java
@@ -1,88 +1,82 @@
-import javax.swing.*;
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
-    public static void initGUI() {
-        Dimension dimension = new Dimension(500, 400);
-        GridLayout gridLayout = new GridLayout(0, 5);
-        JFrame jFrame = new JFrame("BINGO Starter");
 
-        // Numbers for Each button
-        int[] BSet = { 1, 2, 3, 4, 5 };
-        int[] ISet = { 6, 7, 8, 9, 10 };
-        int[] NSet = { 11, 12, 13, 14, 15 };
-        int[] GSet = { 16, 17, 18, 19, 20 };
-        int[] OSet = { 21, 22, 23, 24, 25 };
+    private static Map<Character, Integer> draw() {
+        char[] bingo = { 'B', 'I', 'N', 'G', 'O' };
+        Map<Character, Integer> result = new HashMap<>();
 
-        // Container for the buttons
-        List<JButton> BSetButtons = new ArrayList<>();
-        List<JButton> ISetButtons = new ArrayList<>();
-        List<JButton> NSetButtons = new ArrayList<>();
-        List<JButton> GSetButtons = new ArrayList<>();
-        List<JButton> OSetButtons = new ArrayList<>();
+        char letter = bingo[ThreadLocalRandom.current().nextInt(bingo.length)];
+        int value;
 
-        jFrame.setSize(dimension);
-        jFrame.setLayout(gridLayout);
-        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        // Dynamically create the B buttons. You can create a separate class
-        // to make this more organized. You can better manage your code in this manner.
-        // This is using an enhanced for loop
-
-        for (int i = 0; i < BSet.length; i++) {
-            JButton BButton = new JButton(String.valueOf(BSet[i]));
-            BButton.addActionListener(e -> BButton.setEnabled(false));
-            BSetButtons.add(BButton);
-
-            JButton IButton = new JButton(String.valueOf(ISet[i]));
-            IButton.addActionListener(e -> IButton.setEnabled(false));
-            ISetButtons.add(IButton);
-
-            JButton NButton = new JButton(String.valueOf(NSet[i]));
-            NButton.addActionListener(e -> NButton.setEnabled(false));
-            NSetButtons.add(NButton);
-
-            JButton GButton = new JButton(String.valueOf(GSet[i]));
-            GButton.addActionListener(e -> GButton.setEnabled(false));
-            GSetButtons.add(GButton);
-
-            JButton OButton = new JButton(String.valueOf(OSet[i]));
-            OButton.addActionListener(e -> OButton.setEnabled(false));
-            OSetButtons.add(OButton);
+        switch (letter) {
+            case 'B':
+                value = ThreadLocalRandom.current().nextInt(1, 16);
+                break;
+            case 'I':
+                value = ThreadLocalRandom.current().nextInt(16, 31);
+                break;
+            case 'N':
+                value = ThreadLocalRandom.current().nextInt(31, 46);
+                break;
+            case 'G':
+                value = ThreadLocalRandom.current().nextInt(46, 61);
+                break;
+            case 'O':
+                value = ThreadLocalRandom.current().nextInt(61, 76);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + letter);
         }
 
-        jFrame.getContentPane().add(new JButton("B"));
-        jFrame.getContentPane().add(new JButton("I"));
-        jFrame.getContentPane().add(new JButton("N"));
-        jFrame.getContentPane().add(new JButton("G"));
-        jFrame.getContentPane().add(new JButton("O"));
-
-        // This is also using an enhanced for loop.
-        for (int i = 0; i < BSetButtons.size(); i++) {
-            JButton BButton = BSetButtons.get(i);
-            JButton IButton = ISetButtons.get(i);
-            JButton NButton = NSetButtons.get(i);
-            JButton GButton = GSetButtons.get(i);
-            JButton OButton = OSetButtons.get(i);
-
-            jFrame.getContentPane().add(BButton);
-            jFrame.getContentPane().add(IButton);
-            jFrame.getContentPane().add(NButton);
-            jFrame.getContentPane().add(GButton);
-            jFrame.getContentPane().add(OButton);
-        }
-
-        jFrame.setVisible(true);
-
+        result.put(letter, value);
+        return result;
     }
 
     public static void main(String[] args) {
-        // This is using a lambda implementation.
-        javax.swing.SwingUtilities.invokeLater(Main::initGUI);
+        try (Scanner inputInt = new Scanner(System.in)) {
+
+            Map<Character, Integer> result;
+            char drawLetter;
+            int drawNumber;
+            int rounds = 35;
+            int repeat;
+
+            while (true) {
+                Bingo card = new Bingo();
+                card.fill_cards();
+                card.initGUI();
+                card.setVisibility(true);
+                for (int i = 0; i < rounds; ++i) {
+                    try {
+                        Thread.sleep(2500);
+                    } catch (InterruptedException e) {
+                        // Handle interrupted exception (e.g., log or rethrow)
+                        Thread.currentThread().interrupt(); // Restore the interrupted status
+                    }
+
+                    result = draw();
+                    drawLetter = result.keySet().iterator().next();
+                    drawNumber = result.get(drawLetter);
+                    System.out.println("Letter '" + drawLetter + "' = " + drawNumber);
+                    card.update_card(drawLetter, drawNumber);
+                    if (card.checkStatus()) {
+                        System.out.println("You Win!");
+                        return;
+                    }
+                }
+                System.out.print("You Lose!\nContinue Game (1: Yes, Other: No): ");
+                repeat = inputInt.nextInt();
+                if (repeat == 1) {
+                    card.setVisibility(false);
+                    continue;
+                } else {
+                    break;
+                }
+            }
+        }
     }
 }
-
-// Be creative and have fun coding class!
-// sirmark
